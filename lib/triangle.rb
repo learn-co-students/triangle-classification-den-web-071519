5dd9a146bc66330437e736d2d78df8a62dbd84cb
Your Name <tylercmasterson@gmail.com>
2019-07-25 09:56:58 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Triangle
-  # write code here
+attr_reader :a, :b, :c, :sides
+  def initialize a, b, c
+    @a = a 
+    @b = b 
+    @c = c 
+    @sides = [@a, @b, @c]
+  end
+
+  def kind
+    is_triangle?
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || c == a
+        :isosceles
+      else
+        :scalene
+      end
+  end
+  
+  def is_triangle?
+    true_triangle = [(a + b > c), (b + c > a), (a + c > b)]
+    sides.each do |side|
+      if side <= 0
+        true_triangle << false
+      end
+      true_triangle
+    end
+    if true_triangle.include?(false)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    puts "That's not a triangle, y'all."
+  end
 end
